f0eb02ac63decf66e965df414ef101d538739798
Hal <hgrforrest@gmail.com>
2021-10-08 17:01:37 -0400

    added npm installs and basic router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './styles/scss/styles.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import HomePage from './HomePage'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<h1>Nygma</h1>
+				<Route exact path='/' >
+					<HomePage />
+				</Route>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+
+
+const HomePage = () =>{
+    return(
+        <div>
+            <p>test test</p>
+        </div>
+    )
+}
+
+export default HomePage;
